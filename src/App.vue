6716eaaa4bd45c32c4d0aa899960172598e66cb1
liaixiao <2054519356@qq.com>
2023-05-19 15:03:52 +0800

    feat:监听复杂的响应式数据深度监听和简单的

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
-  <div>廖总有{{ msg }}老婆</div>
-  <button @click="add">+</button>
+  <div>{{ student.name }}身价有{{ student.info.social }}亿</div>
+  <button @click="changName">改名字</button>
+  <button @click="changSocial">改钱数</button>
 </template>
-
 <script setup>
 import { ref, watch } from "vue";
-const msg = ref(10);
-const add = () => {
-  msg.value++;
-};
-// 监听一个响应式数据
-watch(msg, () => {
-  console.log("改变", msg.value);
+const student = ref({
+  name: "廖某",
+  info: {
+    social: 1000000000,
+    age: 18,
+  },
 });
+const changName = () => {
+  student.value.name = "廖总";
+};
+const changSocial = () => {
+  student.value.info.social++;
+};
+// 监听一个简单的响应式数据
+watch(
+  () => student.value.name,
+  () => {
+    console.log("名字发生了改变");
+  }
+);
+// 监听复杂的响应式数据
+watch(
+  () => student.value.info,
+  () => {
+    console.log("钱发送了变化");
+  },
+  {
+    deep: true,
+    // 一开始就触发
+    immediate: true,
+  }
+);
 </script>
 <style lang="scss" scoped></style>
