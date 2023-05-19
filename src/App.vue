4a032ce9f0a7ca29aa8c3a13f2916e3da74dceae
liaixiao <2054519356@qq.com>
2023-05-19 10:18:51 +0800

    feat:初体验

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <button @click="toggle">显示隐藏图片</button>
+  <img v-show="show" alt="Vue logo" src="./assets/preview.gif" />
+  <hr />
+  计数器：{{ count }} <button @click="increment">累加</button>
+  <p>{{ state.name }}</p>
+  <p>{{ state.age }}</p>
+</template>
+<script>
+// ref 就是一个组合式API
+import { reactive, ref } from "vue";
+export default {
+  setup() {
+    // 显示隐藏
+    const show = ref(true);
+    const toggle = () => {
+      show.value = !show.value;
+    };
+    // 计数器
+    const count = ref(0);
+    const increment = () => {
+      count.value++;
+    };
+    // reactive函数
+    // 2 创建响应式数据对象
+    const state = reactive({ name: "廖总", age: 18 });
+    return { show, toggle, count, increment, state };
+    //
+  },
+};
+</script>
